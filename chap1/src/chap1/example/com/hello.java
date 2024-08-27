3d1e22e3e8b7a06579a614a74851236e0ec9946f
soojin <ssj1737@gmail.com>
2024-08-27 15:08:04 +0900

    first

---
+++ chap1/src/chap1/example/com/hello.java
@@ -0,0 +1,12 @@
+package chap1.example.com;
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello, java~~");
+		System.out.println("hello, java~~");
+		System.out.println("hello, java~~");
+	}
+
+}

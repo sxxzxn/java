9bd4633df524121be151e33905a064ad3d4f0348
soojin <ssj1737@gmail.com>
2024-09-09 19:29:35 +0900

    try

--- chap14/src/chap14/NumTest.java
+++ chap14/src/chap14/NumTest.java
@@ -8,13 +8,13 @@ public class NumTest {
 
 		Num n= new Num();
 		 try {
+			 	n.checkNum(99); // 범위를 넘는 값 테스트
 	            n.checkNum(150); // 범위를 넘는 값 테스트
 	            n.checkNum(-50); // 범위를 넘는 값 테스트
-	            n.checkNum(99); // 범위를 넘는 값 테스트
 	        } catch (MinusException | OverException e) {
 	            System.out.println(e.getMessage());
 
 	        }
 
-}
+	}
 }
